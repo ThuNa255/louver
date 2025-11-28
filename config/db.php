5c0cdcfd0df08fd836211469f60315b9d9721376
Noahh0509 <mab30367@gmail.com>
2025-11-28 13:31:50 +0700

    update dbconfig

--- config/db.php
+++ config/db.php
@@ -1,9 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "bagshop";
-
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "bagshop";
+} 
+// Nếu đang chạy trên InfinityFree
+else {
+    $host = "sql100.infinityfree.com";
+    $user = "if0_40541312";
+    $pass = "6nivQz2j2G";
+    $db   = "if0_40541312_bagshop";
+}
 $conn = new mysqli($host, $user, $pass, $db);
 $conn->set_charset("utf8");
 
